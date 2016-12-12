d38bd7647d325d5be3f64e972e1f8a6256aa0c09
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2016-12-12 15:25:45 -0200

    Initial Commit

---
+++ RavenDB/RavenDB/Author.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenDB
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Email { get; set; }
+
+        public List<Post> Post { get; set; }
+    }
+
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public String Title { get; set; }
+
+        public String Article { get; set; }
+
+        public DateTime DtPost { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+    }
+
+    public class Comment
+    {
+        public Guid Id { get; set; }
+
+        public String Title { get; set; }
+
+        public String Text { get; set; }
+
+        public String CommentUser { get; set; }
+
+        public DateTime DtComment { get; set; }
+
+    }
+}
---
+++ RavenDB/RavenDB/Program.cs
@@ -0,0 +1,88 @@
+﻿using Raven.Client.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var documentStore = new DocumentStore();
+            documentStore.Url = "http://localhost:8080";
+            documentStore.Initialize();
+
+            //Cria a base de dados caso necessario
+            documentStore.DatabaseCommands.GlobalAdmin.EnsureDatabaseExists("Post");
+
+            //Armazena o Id para posteriormente seguir o exemplo de alteração e delete
+            var idAuthor = Guid.NewGuid();
+
+            //Cria um novo documento
+            using (var session = documentStore.OpenSession("Post"))
+            {
+
+                var author = new Author()
+                {
+                    Id = idAuthor,
+                    Email = "xpto@teste.com.br",
+                    Name = "Rafael Cruz",
+                    Post = new List<Post>() {
+                        new Post()
+                        {
+                            Id = Guid.NewGuid(),
+                            Article = "Lorem ipsum",
+                            DtPost = DateTime.Now,
+                            Title = "Novo Artigo de Teste",
+                            Comments = new List<Comment>()
+                            {
+                                new Comment()
+                                {
+                                    Title = "Novo Comentário",
+                                    Text = "Lorem Ipsum",
+                                    CommentUser = "User Comment",
+                                    DtComment = DateTime.Now,
+                                    Id = Guid.NewGuid(),
+                                }
+
+                            }
+
+                        }
+                    }
+                };
+
+                session.Store(author);
+                session.SaveChanges();
+            }
+
+            //Alterando uma ou mais propriedades
+            using (var session = documentStore.OpenSession("Post"))
+            {
+                // usamos o método Load para recuperar um documento pelo Id
+                var author = session.Load<Author>($"authors/{idAuthor}");
+
+                author.Name = "Rafael Cruz Alterado";
+                session.SaveChanges();
+            }
+
+            //Deletando um documento
+            using (var session = documentStore.OpenSession("Post"))
+            {
+                // usamos o método Load para recuperar um documento pelo Id
+                var author = session.Load<Author>($"authors/{idAuthor}");
+
+                session.Delete<Author>(author);
+                session.SaveChanges();
+            }
+                        
+            documentStore.Dispose();
+
+            Console.WriteLine("Operação concluída");
+            Console.ReadKey();
+
+        }
+    }
+}
